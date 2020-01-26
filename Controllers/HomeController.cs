1a5e96d96462a1276362c6be2ab4835d820c6bd1
Antonio <azamora@telstock.net>
2020-01-25 20:06:11 -0600

    Poryecto Alumno

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Alumno.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Alumno.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        public HomeController(IWebHostEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(AlumnoModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                GuardarAlumno(model);
+            }
+            else
+            {
+                model.Generos = new List<SelectListItem>
+                {
+                    new SelectListItem {Text = "Masculino", Value = "1Masculino"},
+                    new SelectListItem {Text = "Femenino", Value = "Femenino"}
+                };
+            }
+            return View(model);
+        }
+        public IActionResult Index()
+        {
+            var vm = new AlumnoModel();
+            vm.Generos = new List<SelectListItem>
+            {
+                new SelectListItem {Text = "Masculino", Value = "1Masculino"},
+                new SelectListItem {Text = "Femenino", Value = "Femenino"}
+            };
+            return View(vm);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        private void GuardarAlumno(AlumnoModel model)
+        {
+            string path = _hostingEnvironment.ContentRootPath + "/Alumnos/" + model.RFC + ".txt";
+            if (!System.IO.File.Exists(path))
+            {
+                using (StreamWriter sw = System.IO.File.CreateText(path))
+                {
+                }
+            }
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(path, true))
+            {
+                string s = "Nombre: " + model.Nombre + "| Apellido Paterno: " + model.Apellido_Paterno + "| Apellido Materno" + model.Apellido_Materno + "| Fecha de Nacimiento: " + model.Fecha_Nacimiento + "| Genero: " + model.Genero + "| Fecha de Ingreso: " + model.Fecha_Ingreso + "| Activo: " + model.Activo + "| RFC: " + model.RFC;
+                file.WriteLine(s);
+            }
+        }
+    }
+}
---
+++ Models/AlumnoModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Alumno.Models
+{
+    public class AlumnoModel
+    {
+        [Key]
+        public int IdAlumno { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [MaxLength(50, ErrorMessage = "El nombre no puede ser mayor a 50 caracteres")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "El apellido paterno es obligatorio")]
+        [Display(Name = "Apellido Paterno")]
+        [MaxLength(50, ErrorMessage = "El apellido paterno no puede ser mayor a 50 caracteres")]
+        public string Apellido_Paterno { get; set; }
+        [Display(Name = "Apellido Materno")]
+        [MaxLength(50, ErrorMessage = "El apellido materno no puede ser mayor a 50 caracteres")]
+        public string Apellido_Materno { get; set; }
+        [Range(typeof(DateTime), "1/1/1966", "1/1/2019")]
+        [Required(ErrorMessage = "La fecha de nacimiento es obligatorio")]
+        [Display(Name = "Fecha de Nacimiento")]
+        [DataType(DataType.Date)]
+        public DateTime Fecha_Nacimiento { get; set; }
+        [Required(ErrorMessage = "El genero es obligatorio")]
+        public string Genero { get; set; }
+        [Range(typeof(DateTime), "1/1/1966", "1/1/2021")]
+        [Required(ErrorMessage = "La fecha de ingreso es obligatorio")]
+        [Display(Name = "Fecha de Ingreso")]
+        [DataType(DataType.Date)]
+        public DateTime Fecha_Ingreso { get; set; }
+        public bool Activo { get; set; }
+        [Required(ErrorMessage = "El RFC es obligatorio")]
+        //[RegularExpression(@"/^([A-ZÑ&]{3,4}) ?(?:- ?)?(\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])) ?(?:- ?)?([A-Z\d]{2})([A\d])$/")]
+        public string RFC { get; set; }
+
+        public List<SelectListItem> Generos { get; set; }
+    }
+}
